29175bda354bb1c6f515b03201e45b1b9a2757a3
Borislava Kirova <b.kirova@dopamine.bg>
2021-07-15 09:18:48 +0300

    feat: return api data

--- api/ai/latest.js
+++ api/ai/latest.js
@@ -1,5 +1,10 @@
+const generateRandomAIPost = require('../../utils').generateRandomAIPost;
+
 module.exports = (req, res) => {
+    let item1 = generateRandomAIPost()
+    let item2 = generateRandomAIPost()
+    let item3 = generateRandomAIPost()
     res.json({
-      latest: []
+      latest: [item1, item2, item3]
     });
 };
--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const testRout = require('./api/test');
 const latestRout = require('./api/ai/latest');
 
-app.get('/test', (req, res) => testRout(req, res))
-app.get('/ai/latest', (req, res) => latestRout(req, res))
+app.get('/api/ai/latest', (req, res) => latestRout(req, res))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
