7acad1d9f33774cc2a7de62c048bd0bb3ba697eb
Borislava Kirova <b.kirova@dopamine.bg>
2021-07-15 09:08:47 +0300

    feat: test api

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const testRout = require('./api/test');
+const latestRout = require('./api/latest');
+
+app.get('/test', (req, res) => testRout(req, res))
+app.get('/ai/latest', (req, res) => latestRout(req, res))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
