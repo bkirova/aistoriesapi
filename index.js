2fc4acaa53704512b4b713f791856af204b49e0d
Borislava Kirova <b.kirova@dopamine.bg>
2021-07-15 09:13:06 +0300

    fix: wrong path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const testRout = require('./api/test');
-const latestRout = require('./api/latest');
+const latestRout = require('./api/ai/latest');
 
 app.get('/test', (req, res) => testRout(req, res))
 app.get('/ai/latest', (req, res) => latestRout(req, res))
